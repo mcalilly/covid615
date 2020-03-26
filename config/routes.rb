3d7c0bfed7e24f33e217e898605bdbfacda4833b
Lee McAlilly <leemcalilly@gmail.com>
2020-03-26 00:17:55 -0500

    The home page should go to the nashville dashboard for now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # Public Marketing Pages
- root "marketing#home"
+ root "marketing#nashville"
  get "/about" => "marketing#about", as: "about"
  get "/get-involved" => "marketing#get_involved", as: "get_involved"
  get "/nashville" => "marketing#nashville", as: "nashville"
