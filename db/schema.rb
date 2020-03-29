e7c263e757c03b383a5aebbe379c658eef6fab26
Lee McAlilly <leemcalilly@gmail.com>
2020-03-29 10:39:02 -0500

    Update should include the time as well as the date

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 2020_03_29_130232) do
 
   create_table "updates", force: :cascade do |t|
     t.bigint "county_id", null: false
-    t.date "date"
+    t.date "datetime"
     t.string "source"
     t.integer "cases"
     t.integer "deaths"
