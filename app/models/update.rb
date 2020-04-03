1991de3a544c34a5de2a4d76dcbe9cc3d12b09e4
Lee McAlilly <leemcalilly@gmail.com>
2020-04-03 10:31:07 -0500

    Fix growth rates in the today section for nashville

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,7 +1,7 @@
 class Update < ApplicationRecord
   default_scope { order(date: :desc) }
 
-  before_save :calculate_case_growth_rate
+  after_commit :calculate_case_growth_rate
   # after_commit :calculate_daily_death_growth_rate
 
   belongs_to :county
@@ -11,7 +11,7 @@ class Update < ApplicationRecord
 
     def calculate_case_growth_rate
       current_cases  = self.cases
-      previous_cases = Update.where(date: 1.day.ago).sum(:cases)
+      previous_cases = Update.where(date: (self.date - 1.day)).sum(:cases)
       self.case_growth_rate = (current_cases - previous_cases) / current_cases
     end
 
