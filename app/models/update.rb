e1e583d6526cbf76f803961901152a4cd0d6cb92
Lee McAlilly <leemcalilly@gmail.com>
2020-04-03 11:05:24 -0500

    My last commit accidentally re-wrote 17100262 by Maueez. Restoring that change here

--- app/models/update.rb
+++ app/models/update.rb
@@ -11,14 +11,15 @@ class Update < ApplicationRecord
 
     def calculate_case_growth_rate
       current_cases  = self.cases
-      previous_cases = Update.where(date: (self.date - 1.day)).sum(:cases)
-      self.case_growth_rate = ((current_cases - previous_cases) / current_cases)
+      previous_cases = Update.where(date: (date-1.day ) ).sum(:cases)
+      self.case_growth_rate = ((current_cases - previous_cases) / current_cases)*100
     end
 
     def calculate_daily_death_growth_rate
       # do math to calculate daily death growth rate
     end
 end
+
 
 # current_update  = Update.last
 # current_cases   = current_update.cases
