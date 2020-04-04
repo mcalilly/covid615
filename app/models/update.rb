73a269171241b77e9f639e57cc6fb86cd68ea605
Lee McAlilly <leemcalilly@gmail.com>
2020-04-04 12:35:10 -0500

    Fix edge cases on calculating growth rates when something is 0

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,42 +1,99 @@
 class Update < ApplicationRecord
   default_scope { order(date: :desc) }
 
-  after_commit :calculate_case_growth_rate
-  # after_commit :calculate_daily_death_growth_rate
+  after_commit :calculate_daily_case_growth_rate
+  after_commit :calculate_daily_death_growth_rate
 
   belongs_to :county
   validates :date, uniqueness: true
 
   private
 
-    def calculate_case_growth_rate
+    def calculate_daily_case_growth_rate
       current_update  = Update.where(date: self.date)
       previous_update = Update.where(date: self.date-1.day)
       next_update     = Update.where(date: self.date+1.day)
 
-      if previous_update.present?
-        current_update_cases  = current_update.sum(:cases).to_f
-        previous_update_cases = previous_update.sum(:cases).to_f
-        current_update_case_growth_rate = ((current_update_cases - previous_update_cases) / previous_update_cases) * 100
-        self.update_columns(case_growth_rate: current_update_case_growth_rate)
+      current_update_cases  = current_update.sum(:cases).to_f
+      previous_update_cases = previous_update.sum(:cases).to_f
+      next_update_cases = next_update.sum(:cases).to_f
+
+      current_update_case_growth_rate = ((current_update_cases - previous_update_cases) / previous_update_cases) * 100
+      next_update_cases_growth_rate = ((next_update_cases - current_update_cases) / current_update_cases) * 100
+
+      if previous_update.present? && next_update.empty?
+        if current_update_cases == 0
+          self.update_columns(case_growth_rate: nil)
+        else
+          self.update_columns(case_growth_rate: current_update_case_growth_rate)
+        end
       end
 
       if next_update.present? && previous_update.present?
-        next_update_cases = next_update.sum(:cases).to_f
-        next_update_case_growth_rate = ((next_update_cases - current_update_cases) / current_update_cases) * 100
-        next_update.update_all(case_growth_rate: next_update_case_growth_rate)
+        if current_update_cases == 0
+          self.update_columns(case_growth_rate: nil)
+          next_update.update_all(case_growth_rate: nil)
+        elsif current_update_cases > 0 && next_update_cases == 0
+          self.update_columns(case_growth_rate: current_update_case_growth_rate)
+          next_update.update_all(case_growth_rate: nil)
+        else
+          self.update_columns(case_growth_rate: current_update_case_growth_rate)
+          next_update.update_all(case_growth_rate: next_update_cases_growth_rate)
+        end
       end
 
-      if next_update.present? != previous_update.present?
-        self.update_columns(case_growth_rate: 100.00)
-        next_update_cases = next_update.sum(:cases).to_f
-        current_update_cases  = current_update.sum(:cases).to_f
-        next_update_case_growth_rate = ((next_update_cases - current_update_cases) / current_update_cases) * 100
-        next_update.update_all(case_growth_rate: next_update_case_growth_rate)
+      if next_update.present? && previous_update.empty?
+        if current_update_cases == 0 || next_update_cases == 0
+          self.update_columns(case_growth_rate: nil)
+          next_update.update_all(case_growth_rate: nil)
+        else
+          self.update_columns(case_growth_rate: nil)
+          next_update.update_all(case_growth_rate: next_update_cases_growth_rate)
+        end
       end
     end
 
     def calculate_daily_death_growth_rate
-      # do math to calculate daily death growth rate
+      current_update  = Update.where(date: self.date)
+      previous_update = Update.where(date: self.date-1.day)
+      next_update     = Update.where(date: self.date+1.day)
+
+      current_update_deaths  = current_update.sum(:deaths).to_f
+      previous_update_deaths = previous_update.sum(:deaths).to_f
+      next_update_deaths = next_update.sum(:deaths).to_f
+
+      current_update_death_growth_rate = ((current_update_deaths - previous_update_deaths) / previous_update_deaths) * 100
+      next_update_deaths_growth_rate = ((next_update_deaths - current_update_deaths) / current_update_deaths) * 100
+
+      if previous_update.present? && next_update.empty?
+        if current_update_deaths == 0
+          self.update_columns(death_growth_rate: nil)
+        else
+          self.update_columns(death_growth_rate: current_update_death_growth_rate)
+        end
+      end
+
+      if next_update.present? && previous_update.present?
+        if current_update_deaths == 0
+          self.update_columns(death_growth_rate: nil)
+          next_update.update_all(death_growth_rate: nil)
+        elsif current_update_deaths > 0 && next_update_deaths == 0
+          self.update_columns(death_growth_rate: current_update_death_growth_rate)
+          next_update.update_all(death_growth_rate: nil)
+        else
+          self.update_columns(death_growth_rate: current_update_death_growth_rate)
+          next_update.update_all(death_growth_rate: next_update_deaths_growth_rate)
+        end
+      end
+
+      if next_update.present? && previous_update.empty?
+        if current_update_deaths == 0 || next_update_deaths == 0
+          self.update_columns(death_growth_rate: nil)
+          next_update.update_all(death_growth_rate: nil)
+        else
+          self.update_columns(death_growth_rate: nil)
+          next_update.update_all(death_growth_rate: next_update_deaths_growth_rate)
+        end
+      end
     end
 end
