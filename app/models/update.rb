7dcf54a0e779d77ca9480069cbd604b732b24e44
Lee McAlilly <leemcalilly@gmail.com>
2020-04-04 10:30:12 -0500

    If the current update changes, then we also need to change the growth rate of the next update as well

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,7 +1,7 @@
 class Update < ApplicationRecord
   default_scope { order(date: :desc) }
 
-  before_save :calculate_case_growth_rate
+  after_commit :calculate_case_growth_rate
   # after_commit :calculate_daily_death_growth_rate
 
   belongs_to :county
@@ -10,9 +10,20 @@ class Update < ApplicationRecord
   private
 
     def calculate_case_growth_rate
-      current_cases  = Update.where(date: self.date).sum(:cases).to_f
-      previous_cases = Update.where(date: self.date-1.day ).sum(:cases).to_f
-      self.case_growth_rate = ((current_cases - previous_cases) / previous_cases) * 100
+      current_update  = Update.where(date: self.date)
+      previous_update = Update.where(date: self.date-1.day)
+      next_update     = Update.where(date: self.date+1.day)
+
+      current_update_cases  = current_update.sum(:cases).to_f
+      previous_update_cases = previous_update.sum(:cases).to_f
+      current_update_case_growth_rate = ((current_update_cases - previous_update_cases) / previous_update_cases) * 100
+      self.update_columns(case_growth_rate: current_update_case_growth_rate)
+
+      if next_update.present?
+        next_update_cases = next_update.sum(:cases).to_f
+        next_update_case_growth_rate = ((next_update_cases - current_update_cases) / current_update_cases) * 100
+        self.next_update.update_columns(case_growth_rate: next_update_case_growth_rate)
+      end
     end
 
     def calculate_daily_death_growth_rate
