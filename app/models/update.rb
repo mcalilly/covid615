7e7cca70ea34fc0bc7ec9f004628498a4ea95a8c
Lee McAlilly <leemcalilly@gmail.com>
2020-04-05 12:11:00 -0500

    Can't divide by zero

--- app/models/update.rb
+++ app/models/update.rb
@@ -14,23 +14,29 @@ class Update < ApplicationRecord
   private
 
     def calculate_total_cases
-      updates_through_today = Update.where(date: 100.years.ago..self.date)
-      total_cases_through_today = updates_through_today.sum(:cases).to_f
-      self.update_columns(total_cases: total_cases_through_today)
+      if self.present?
+        updates_through_today = Update.where(date: 100.years.ago..self.date)
+        total_cases_through_today = updates_through_today.sum(:cases).to_f
+        self.update_columns(total_cases: total_cases_through_today)
+      end
     end
 
     def calculate_total_deaths
-      updates_through_today = Update.where(date: 100.years.ago..self.date)
-      total_deaths_through_today = updates_through_today.sum(:deaths).to_f
-      self.update_columns(total_deaths: total_deaths_through_today)
+      if self.present?
+        updates_through_today = Update.where(date: 100.years.ago..self.date)
+        total_deaths_through_today = updates_through_today.sum(:deaths).to_f
+        self.update_columns(total_deaths: total_deaths_through_today)
+      end
     end
 
     def calculate_average_death_rate
-      updates_through_today = Update.where(date: 100.years.ago..self.date)
-      total_cases_through_today = updates_through_today.sum(:cases).to_f
-      total_deaths_through_today = updates_through_today.sum(:deaths).to_f
-      current_average_death_rate = total_deaths_through_today / total_cases_through_today
-      self.update_columns(average_death_rate: current_average_death_rate)
+      if self.present?
+        updates_through_today = Update.where(date: 100.years.ago..self.date)
+        total_cases_through_today = updates_through_today.sum(:cases).to_f
+        total_deaths_through_today = updates_through_today.sum(:deaths).to_f
+        current_average_death_rate = total_deaths_through_today / total_cases_through_today
+        self.update_columns(average_death_rate: current_average_death_rate)
+      end
     end
 
     def calculate_new_cases_growth_rate
@@ -38,26 +44,27 @@ class Update < ApplicationRecord
       previous_update = Update.where(date: self.date-1.day)
       next_update     = Update.where(date: self.date+1.day)
 
-      current_update_cases  = current_update.sum(:cases).to_f
-      previous_update_cases = previous_update.sum(:cases).to_f
-      next_update_cases = next_update.sum(:cases).to_f
+      current_update_new_cases  = current_update.sum(:cases).to_f
+      previous_update_new_cases = previous_update.sum(:cases).to_f
+      next_update_new_cases = next_update.sum(:cases).to_f
 
-      current_update_case_growth_rate = (current_update_cases - previous_update_cases) / previous_update_cases
-      next_update_cases_growth_rate = (next_update_cases - current_update_cases) / current_update_cases
+      current_update_case_growth_rate = (current_update_new_cases - previous_update_new_cases) / previous_update_new_cases
+      next_update_cases_growth_rate = (next_update_new_cases - current_update_new_cases) / current_update_new_cases
 
       if previous_update.present? && next_update.empty?
-        if current_update_cases == 0
+        if current_update_new_cases == 0 || previous_update_new_cases == 0
           self.update_columns(new_cases_growth_rate: nil)
         else
+          binding.pry
           self.update_columns(new_cases_growth_rate: current_update_case_growth_rate)
         end
       end
 
       if next_update.present? && previous_update.present?
-        if current_update_cases == 0
+        if current_update_new_cases == 0
           self.update_columns(new_cases_growth_rate: nil)
           next_update.update_all(new_cases_growth_rate: nil)
-        elsif current_update_cases > 0 && next_update_cases == 0
+        elsif current_update_new_cases > 0 && next_update_new_cases == 0
           self.update_columns(new_cases_growth_rate: current_update_case_growth_rate)
           next_update.update_all(new_cases_growth_rate: nil)
         else
@@ -67,7 +74,7 @@ class Update < ApplicationRecord
       end
 
       if next_update.present? && previous_update.empty?
-        if current_update_cases == 0 || next_update_cases == 0
+        if current_update_new_cases == 0 || next_update_new_cases == 0
           self.update_columns(new_cases_growth_rate: nil)
           next_update.update_all(new_cases_growth_rate: nil)
         else
