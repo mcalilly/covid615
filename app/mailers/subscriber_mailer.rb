91d4994374f352e5da203d790edd74c0c433b061
Lee McAlilly <leemcalilly@gmail.com>
2020-07-08 00:07:00 -0500

    Add a dailer mailer task

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,2 +1,19 @@
 class SubscriberMailer < ApplicationMailer
+
+  def send_daily_updates
+    @subscribers = Subscribers.all
+
+    @todays_update = Update.last
+    @yesterdays_update = Update.where(date: @current_update.date-1.day)
+
+    @todays_cases = @todays_update.new_cases
+    @yesterdays_cases = @yesterdays_update.new_cases
+    @new_deaths = @todays_update.new_deaths
+    @total_deaths = @todays_update.total_deaths
+
+    @subscribers.each do |subscriber|
+      mail(to: subscriber.email, subject:  "#{Time.now.in_time_zone("Central Time (US & Canada)").strftime("%B %e, %Y")} • Covid-19 Daily Update for Nashville, TN")
+    end
+  end
+
 end
